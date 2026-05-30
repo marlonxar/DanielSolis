a614b1f693be22b537d78a41088daaa606ea501f
marlonxar <xar.marlon@gmail.com>
2026-05-30 10:54:16 -0600

    Porfatolio and Contacto added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PreviewComponent } from './pages/preview/preview.component';
 import { RetroComponent } from './pages/retro/retro.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'preview',
-    component: PreviewComponent,
-  },
-  {
-    path: 'retro',
     component: RetroComponent,
+  },
+  {
+    path: 'portafolio',
+    component: PortafolioComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
   },
 ];
 
